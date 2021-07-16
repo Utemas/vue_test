b49c7f3f858809012f143922ba018c8c5fff7938
Ada August <2562817585@qq.com>
2021-07-16 14:21:55 +0800

    first_test

---
+++ src/components/VLink.vue
@@ -0,0 +1,14 @@
+<style>
+  @import "../assets/css/vlink.css";
+</style>
+<template>
+  <a
+    v-bind:href="href"
+    v-bind:class="{ active: isActive }"
+    v-on:click="go"
+  >
+    <slot></slot>
+  </a>
+</template>
+
+<script src="../assets/js/vlink.js"></script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import TopDiv from './vue/TopDiv'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  ...TopDiv
+})
