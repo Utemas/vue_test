464a30647b57384fce11e2ad97c8b0db4ff3323a
Ada August <2562817585@qq.com>
2021-07-19 21:34:39 +0800

    29219719

---
+++ src/vue/Index.vue
@@ -0,0 +1,17 @@
+<style>
+  @import "../assets/css/Index.css";
+</style>
+<template>
+  <div id="app" class="background" >
+    <div class="title">
+      <span>Welcome</span>
+      <p>This is a Synthetical System, click 'Skip' to start.</p>
+    </div>
+    <div class="loading"></div>
+    <div class="bottomButton"><a href="#">SKIP>></a></div>
+  </div>
+</template>
+
+<script src="../assets/js/Index.js"></script>
+
+<style></style>
